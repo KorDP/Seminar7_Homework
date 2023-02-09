f2dd7b8eee512f82fde8ee658598b99d6789b0d9
KorDP <nukedmitry@gmail.com>
2023-02-09 19:44:13 +0300

    done

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] CreateMatrixRndInt(int row, int col)
+{
+    double[,] matr = new double[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = Math.Round(rnd.NextDouble() * rnd.Next(-10, 10), 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4}, ");
+            else Console.Write($"{matrix[i,j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] matrix = CreateMatrixRndInt(5,4);
+PrintMatrix(matrix);
---
+++ task52/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matr = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+    Console.WriteLine();
+}
+
+
+void ColAvarage(int[,] matr)
+{
+    Console.Write("[");
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+    double result = 0;
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            result += matr[j,i];
+        }
+    if (i < matr.GetLength(1) -1 ) Console.Write(result / matr.GetLength(0) + ", ");
+    else Console.Write(result / matr.GetLength(0) + "]");
+    }
+}
+
+
+int[,] matrix = CreateMatrixRndInt(4, 5, 1, 10);
+PrintMatrix(matrix);
+Console.Write("Среднее арифметическое каждого столбца: ");
+ColAvarage(matrix);
